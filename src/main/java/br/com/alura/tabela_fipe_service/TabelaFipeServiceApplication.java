412f6356fc63d457f6e8016800564fa0212d7350
AshileySabah <ashileysabrina2002@gmail.com>
2024-10-04 11:01:45 -0300

    feat: initial files

---
+++ src/main/java/br/com/alura/tabela_fipe_service/TabelaFipeServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.tabela_fipe_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabelaFipeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabelaFipeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/tabela_fipe_service/TabelaFipeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.tabela_fipe_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabelaFipeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
